aa1e9877db72eb1ca8953f32ef2fd4d6008f14de
FreakingLoki <jesseheald2010@gmail.com>
2023-02-04 07:53:42 -0500

    add name generator

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ This program will output a randomized message every time it is run. That message
 the program will generate a Fantasy world character (think DnD or video game) sheet
 This character sheet will include some basic stats, a name, and a short bio (enlist ChatGPT to write a few?)
 
+the output will start with a name followed by a short bio then the stats for the character generated.
+
 ### Stats:
   * Luck (has an affect on all other rolls)
   * Strength
--- script.js
+++ script.js
@@ -4,41 +4,64 @@
 const genLuck = () => {
     let luck = Math.floor(Math.random() * 50);
     return luck;
-}
+};
 
 //roll for strength and add luck
 const genStrength = luck => {
     let numSTR = Math.floor(Math.random() * 50);
     numSTR += luck;
     return numSTR;
-}
+};
 
 //  roll for vitality, add 1/2 STR and add luck
 const genVitality = (luck, strength) => {
     let numVIT = Math.floor(Math.random() * 50);
     numVIT += Math.floor(((0.5 * strength) + luck));
     return numVIT
-}
+};
 
 // roll for intelligence and add luck
 const genIntelligence = luck => {
     let numINT = Math.floor(Math.random() * 50);
     numINT += luck;
     return numINT;
-}
+};
 
 // roll for wisdom, add 1/2 INT
 const genWisdom = (luck, intelligence) => {
     let numWIS = Math.floor(Math.random() * 50);
     numWIS +=  Math.floor(((0.5 * intelligence) + luck));
     return numWIS;
-}
+};
 
 const genCharisma = luck => {
     let numCHA = Math.floor(Math.random() * 50);
     numCHA += Math.floor((0.25 * luck));
     return numCHA
-}
+};
+
+//name generator
+const genName = () => {
+    //arrays of names
+    let firstNames = [
+        'Daryndel', 'Azura', 'Deliadorn', 'Hrothgar', 'Faelivrin',
+        'Lothlorien', 'Tathariel', 'Eilinel', 'Galathil', 'Arweniel',
+        'Eadwynn', 'Nessa', 'Rhudaur', 'Thrainor', 'Melwasul'
+    ];
+    let lastNames = [
+        'Fireheart', 'Starweaver', 'Snowfoot', 'Ironfist', 'Moonblade',
+        'of the Wood', 'Stormchaser', 'Moonlit', 'Forestwhisper', 'Shadowblade', 
+        'Oceanheart', 'Wildwood', 'Watersinger', 'Wanderer', 'Thundercaller'
+    ];
+
+    //randomize numbers to get ready to select name
+    let numOne = Math.floor(Math.random() * 16);
+    let numTwo = Math.floor(Math.random() * 16);
+
+    let charName = firstNames[numOne] + ' ' + lastNames[numTwo];
+    return charName;
+
+};
 
 //get values for the stats
 const LCK = genLuck();
@@ -47,8 +70,10 @@ const VIT = genVitality(LCK, STR);
 const INT = genIntelligence(LCK);
 const WIS = genWisdom(LCK, INT);
 const CHA = genCharisma(LCK);
+//generate a name
+const charName = genName();
 
-
+console.log(`Name: ${charName}`)
 console.log(`Luck or LCK: ${LCK}`);
 console.log(`Strength or STR: ${STR}`);
 console.log(`Vitality or VIT: ${VIT}`);
