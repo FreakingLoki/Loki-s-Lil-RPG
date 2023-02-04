20bc5a4c512a82409dc0ee947a82a9b3e86d5ba0
FreakingLoki <jesseheald2010@gmail.com>
2023-02-04 08:04:01 -0500

    add fidelity to name generator

--- script.js
+++ script.js
@@ -2,40 +2,40 @@
 
 // luck has an effect on all other stat rolls
 const genLuck = () => {
-    let luck = Math.floor(Math.random() * 50);
+    let luck = Math.floor(Math.random() * 51);
     return luck;
 };
 
 //roll for strength and add luck
 const genStrength = luck => {
-    let numSTR = Math.floor(Math.random() * 50);
+    let numSTR = Math.floor(Math.random() * 51);
     numSTR += luck;
     return numSTR;
 };
 
 //  roll for vitality, add 1/2 STR and add luck
 const genVitality = (luck, strength) => {
-    let numVIT = Math.floor(Math.random() * 50);
+    let numVIT = Math.floor(Math.random() * 51);
     numVIT += Math.floor(((0.5 * strength) + luck));
     return numVIT
 };
 
 // roll for intelligence and add luck
 const genIntelligence = luck => {
-    let numINT = Math.floor(Math.random() * 50);
+    let numINT = Math.floor(Math.random() * 51);
     numINT += luck;
     return numINT;
 };
 
 // roll for wisdom, add 1/2 INT
 const genWisdom = (luck, intelligence) => {
-    let numWIS = Math.floor(Math.random() * 50);
+    let numWIS = Math.floor(Math.random() * 51);
     numWIS +=  Math.floor(((0.5 * intelligence) + luck));
     return numWIS;
 };
 
 const genCharisma = luck => {
-    let numCHA = Math.floor(Math.random() * 50);
+    let numCHA = Math.floor(Math.random() * 51);
     numCHA += Math.floor((0.25 * luck));
     return numCHA
 };
@@ -44,19 +44,24 @@ const genCharisma = luck => {
 const genName = () => {
     //arrays of names
     let firstNames = [
-        'Daryndel', 'Azura', 'Deliadorn', 'Hrothgar', 'Faelivrin',
-        'Lothlorien', 'Tathariel', 'Eilinel', 'Galathil', 'Arweniel',
-        'Eadwynn', 'Nessa', 'Rhudaur', 'Thrainor', 'Melwasul'
+        'Daryndel', 'Azura', 'Deliadorn', 'Hrothgar', 'Faelivrin', 'Lothlorien', 
+        'Tathariel', 'Eilinel', 'Galathil', 'Arweniel', 'Eadwynn', 'Nessa', 'Rhudaur', 
+        'Thrainor', 'Melwasul', 'Elara', 'Thorne', 'Kael', 'Niamh', 'Aelric', 
+        'Seraphina', 'Evangeline', 'Orion', 'Isolde', 'Lorcan', 'Calla', 'Lysandra', 
+        'Dante', 'Ariadne', 'Galen'
     ];
     let lastNames = [
         'Fireheart', 'Starweaver', 'Snowfoot', 'Ironfist', 'Moonblade',
         'of the Wood', 'Stormchaser', 'Moonlit', 'Forestwhisper', 'Shadowblade', 
-        'Oceanheart', 'Wildwood', 'Watersinger', 'Wanderer', 'Thundercaller'
+        'Oceanheart', 'Wildwood', 'Watersinger', 'Wanderer', 'Thundercaller',
+        'Shadowhunter', 'Stormcaller', 'Nightingale', 'Bloodhound', 'Silverclaw',
+        'Frostheart', 'Wildwood', 'Thunderstrike', 'Blackthorn', 'Moonblade', 
+        'Starweaver', 'Ironfist', 'Fireheart', 'Riverstone', 'Windrider'
     ];
 
     //randomize numbers to get ready to select name
-    let numOne = Math.floor(Math.random() * 16);
-    let numTwo = Math.floor(Math.random() * 16);
+    let numOne = Math.floor(Math.random() * (firstNames.length +1));
+    let numTwo = Math.floor(Math.random() * (lastNames.length +1));
 
     let charName = firstNames[numOne] + ' ' + lastNames[numTwo];
     return charName;
